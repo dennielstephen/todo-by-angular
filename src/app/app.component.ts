0d76cdb7972e030bdb02c4bcf7fada3499e3b42e
dennielstephen <dennielstephen@gmail.com>
2020-04-19 18:46:42 +0800

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
+import { TodoItem } from './interfaces/todo-item';
+
 @Component({
   selector: 'app-root',
   template: `
     <h1>
-      Welcome to {{ title }}
+      Welcome to {{ title }}!
     </h1>
 
     <app-input-button-unit></app-input-button-unit>
 
     <ul>
-      <li *ngFor="let todoItem of todoList">
-        <app-todo-item [item]="todoItem></app-todo-item>
-      </li>
-    </ul>
+  <li *ngFor="let todoItem of todoList">
+    <app-todo-item [item]="todoItem"></app-todo-item>
+  </li>
+</ul>
   `,
   styleUrls: ['./app.component.css']
 })
--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { TodoItem } from '../interfaces/todo-item';
 
 @Component({
   selector: 'app-todo-item',
@@ -8,6 +9,7 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./todo-item.component.css']
 })
 export class TodoItemComponent implements OnInit {
+  @Input() item: TodoItem;
 
   constructor() { }
 
